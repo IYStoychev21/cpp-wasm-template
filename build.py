65e19f7528ed76288b4ceb2c51d20bc6aafb5965
IYStoychev21 <IYStoychev21@codingburgas.bg>
2023-11-19 11:19:21 +0200

    Fix include dirs bug and add requirements.txt file

--- build.py
+++ build.py
@@ -4,6 +4,8 @@ import glob
 import subprocess
 
 def load(path_to_config):
+    print("-------------- LOADING CONFIG FILE --------------")
+
     with open(path_to_config, "r") as stream:
         try:
             return yaml.safe_load(stream)
@@ -11,27 +13,34 @@ def load(path_to_config):
             print(exc)
 
 def load_src_files(path_to_src):
+    print("-------------- LOADING SRC FILES --------------")
+
     return glob.glob(f"{path_to_src}/**.cpp", recursive = True)
 
-def compile_to_objs(src_files, inter_dir):
+def compile_to_objs(src_files, include_dir, inter_dir):
+    print("-------------- COMPILING SRC FILES --------------")
+
     subprocess.run(f"emsdk.bat activate latest", shell=True)
     
     for i in src_files:
         file_name = i.split('\\')[-1][0:-4]
-        subprocess.run(f"emcc {i} -c -o {inter_dir}/{file_name}.o", shell=True)
+        include = " -I".join(include_dir)
+        subprocess.run(f"emcc -I{include} {i} -c -o {inter_dir}/{file_name}.o", shell=True)
 
 def load_objs(int_path):
+    print("-------------- LOADING OBJ FILES --------------")
+
     return glob.glob(f"{int_path}/**.o", recursive = True)
 
-def link_to_wasm(objs, output_dir, include_dir, lib_dir, libs):
+def link_to_wasm(objs, output_dir):
+    print("-------------- LINKING OBJ FILES --------------")
+
     obj = " ".join(objs)
-    include = " ".join(include_dir)
-    lib_dirs = " ".join(lib_dir)
-    libs = " ".join(libs)
-    
-    subprocess.run(f"emcc {obj} -I{include} -o {output_dir}/main.js", shell=True)
+    subprocess.run(f"emcc {obj} -o {output_dir}/main.js", shell=True)
 
 def clean_up(output_dir): 
+    print("-------------- DELETING JS FILE --------------")
+
     new_dir = output_dir.replace("/", "\\")
     subprocess.run(f"del {new_dir}\\main.js", shell=True)
 
@@ -39,9 +48,9 @@ def main():
     props = load(sys.argv[1])
     src_files = load_src_files(props["SourceDir"])
 
-    compile_to_objs(src_files, props["IntBins"])
+    compile_to_objs(src_files, props["IncludeDirs"], props["IntBins"])
     objs = load_objs(props["IntBins"])
-    link_to_wasm(objs, props["OutputDir"], props["IncludeDirs"], props.get("LibDir", ""), props.get("Libs", ""))
+    link_to_wasm(objs, props["OutputDir"])
 
     clean_up(props["OutputDir"])
 
